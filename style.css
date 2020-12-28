572624ca0be31523b1bccc48e84986c2dc1dc037
happy-coding-0036 <emiko0036@hotmail.com>
2020-12-28 04:32:55 -0500

    added too many files

---
+++ style.css
@@ -0,0 +1,28 @@
+
+
+.display {
+    display: grid;
+    justify-content: center; 
+}
+
+.wrap {
+    display: grid;
+    grid-template-columns: repeat(3, 100px);
+    grid-template-rows: repeat(3, 100px);
+    gap: 10px;
+    
+ 
+    border: blue solid 3px;
+    width: 340px;
+    justify-content: center;  
+  }
+
+.grid {
+    border: black solid 3px;
+    text-align: center;
+  
+}
+
+.gameButton {
+    height: 50px;
+}
